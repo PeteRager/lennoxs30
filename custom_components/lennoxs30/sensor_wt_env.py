f08cfe86dee0a8c3c74c507bab0377b9f9befea7
PeteRager <76050312+PeteRager@users.noreply.github.com>
2023-10-05 14:31:08 -0400

    Weather Environment Sensors

---
+++ custom_components/lennoxs30/sensor_wt_env.py
@@ -0,0 +1,225 @@
+"""Support for Lennoxs30 outdoor temperature sensor"""
+# pylint: disable=global-statement
+# pylint: disable=broad-except
+# pylint: disable=unused-argument
+# pylint: disable=line-too-long
+# pylint: disable=invalid-name
+import logging
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.components.sensor import SensorStateClass, SensorDeviceClass
+from homeassistant.const import PERCENTAGE, UnitOfTemperature, UnitOfSpeed
+
+from lennoxs30api import lennox_system
+
+from . import Manager
+from .base_entity import S30BaseEntityMixin
+from .const import LENNOX_DOMAIN
+from .helpers import helper_create_system_unique_id
+
+_LOGGER = logging.getLogger(__name__)
+
+
+lennox_wt_env_sensors = [
+    {
+        "name": "wt env airquality",
+        "input": "wt_env_airQuality",
+        "availability_attribute": "wt_is_valid",
+        "unavailable_value": "error",
+    },
+    {
+        "name": "wt env tree",
+        "input": "wt_env_tree",
+        "availability_attribute": "wt_is_valid",
+        "unavailable_value": "error",
+    },
+    {
+        "name": "wt env weed",
+        "input": "wt_env_weed",
+        "availability_attribute": "wt_is_valid",
+        "unavailable_value": "error",
+    },
+    {
+        "name": "wt env grass",
+        "input": "wt_env_grass",
+        "availability_attribute": "wt_is_valid",
+        "unavailable_value": "error",
+    },
+    {
+        "name": "wt env mold",
+        "input": "wt_env_mold",
+        "availability_attribute": "wt_is_valid",
+        "unavailable_value": "error",
+    },
+    {
+        "name": "wt env uv index",
+        "input": "wt_env_uv_index",
+        "availability_attribute": "wt_is_valid",
+        "unavailable_value": "error",
+    },
+    {
+        "name": "wt env humidity",
+        "input": "wt_env_humidity",
+        "availability_attribute": "wt_is_valid",
+        "unavailable_value": "error",
+        "precision": 0,
+        "state_class": SensorStateClass.MEASUREMENT,
+        "device_class": SensorDeviceClass.HUMIDITY,
+        "uom": PERCENTAGE,
+    },
+    {
+        "name": "wt env cloud coverage",
+        "input": "wt_env_cloudCoverage",
+        "availability_attribute": "wt_is_valid",
+        "unavailable_value": "error",
+        "precision": 0,
+        "state_class": SensorStateClass.MEASUREMENT,
+        "uom": PERCENTAGE,
+    },
+]
+
+lennox_wt_env_sensors_us = [
+    {
+        "name": "wt env wind speed",
+        "input": "wt_env_windSpeed",
+        "availability_attribute": "wt_is_valid",
+        "unavailable_value": "error",
+        "precision": 1,
+        "state_class": SensorStateClass.MEASUREMENT,
+        "device_class": SensorDeviceClass.SPEED,
+        "uom": UnitOfSpeed.MILES_PER_HOUR,
+    },
+    {
+        "name": "wt env dewpoint",
+        "input": "wt_env_dewpoint",
+        "availability_attribute": "wt_is_valid",
+        "unavailable_value": "error",
+        "precision": 1,
+        "state_class": SensorStateClass.MEASUREMENT,
+        "device_class": SensorDeviceClass.TEMPERATURE,
+        "uom": UnitOfTemperature.FAHRENHEIT,
+    },
+]
+
+lennox_wt_env_sensors_metric = [
+    {
+        "name": "wt env wind speed",
+        "input": "wt_env_windSpeedK",
+        "availability_attribute": "wt_is_valid",
+        "unavailable_value": "error",
+        "precision": 1,
+        "state_class": SensorStateClass.MEASUREMENT,
+        "device_class": SensorDeviceClass.SPEED,
+        "uom": UnitOfSpeed.KILOMETERS_PER_HOUR,
+    },
+    {
+        "name": "wt env dewpoint",
+        "input": "wt_env_dewpointC",
+        "availability_attribute": "wt_is_valid",
+        "unavailable_value": "error",
+        "precision": 1,
+        "state_class": SensorStateClass.MEASUREMENT,
+        "device_class": SensorDeviceClass.TEMPERATURE,
+        "uom": UnitOfTemperature.CELSIUS,
+    },
+]
+
+
+class WTEnvSensor(S30BaseEntityMixin, SensorEntity):
+    """Class for Lennox S40 WTEnvSensor Sensors."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        manager: Manager,
+        system: lennox_system,
+        sensor_dict: dict,
+    ):
+        super().__init__(manager, system)
+        self._hass: HomeAssistant = hass
+        self._sensor_dict: dict = sensor_dict
+        self._myname: str = self._system.name + " " + sensor_dict["name"]
+        self._system_attr: str = sensor_dict["input"]
+        self._unavailable_value: str = sensor_dict.get("unavailable_value")
+        self._availability_attribute: str = sensor_dict.get("availability_attribute")
+        self._precision: int = sensor_dict.get("precision")
+        self._state_class = sensor_dict.get("state_class")
+        self._device_class = sensor_dict.get("device_class")
+        self._uom = sensor_dict.get("uom")
+        self._entity_category = sensor_dict.get("entity_category")
+
+    async def async_added_to_hass(self) -> None:
+        """Run when entity about to be added to hass."""
+        _LOGGER.debug("async_added_to_hass WTEnvSensor myname [%s]", self._myname)
+        attribs = []
+        attribs.append(self._system_attr)
+        if self._availability_attribute is not None:
+            attribs.append(self._availability_attribute)
+        self._system.registerOnUpdateCallback(self.sensor_value_update, attribs)
+        await super().async_added_to_hass()
+
+    def sensor_value_update(self):
+        """Callback to execute on data change"""
+        if _LOGGER.isEnabledFor(logging.DEBUG):
+            _LOGGER.debug("sensor_value_update WTEnvSensor myname [%s]", self._myname)
+        self.schedule_update_ha_state()
+
+    @property
+    def unique_id(self) -> str:
+        return helper_create_system_unique_id(
+            self._system,
+            self._system_attr,
+        )
+
+    @property
+    def name(self):
+        return self._myname
+
+    @property
+    def native_value(self):
+        value = getattr(self._system, self._system_attr)
+        if self._state_class is None:
+            return value
+        try:
+            return round(float(value), self._precision)
+        except ValueError as e:
+            _LOGGER.warning(
+                "native_value myname [%s] sensor value [%s] exception: [%s]",
+                self._myname,
+                value,
+                e,
+            )
+        return None
+
+    @property
+    def state_class(self):
+        return self._state_class
+
+    @property
+    def device_class(self):
+        return self._device_class
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return device info."""
+        return {
+            "identifiers": {(LENNOX_DOMAIN, self._system.unique_id)},
+        }
+
+    @property
+    def native_unit_of_measurement(self):
+        return self._uom
+
+    @property
+    def available(self) -> bool:
+        if getattr(self._system, self._system_attr) == self._unavailable_value:
+            return False
+        if getattr(self._system, self._availability_attribute) is False:
+            return False
+        return super().available
+
+    @property
+    def entity_category(self):
+        return self._entity_category
---
+++ tests/test_sensor_wt_env.py
@@ -0,0 +1,136 @@
+"""Test BLE Sensors"""
+# pylint: disable=line-too-long
+import logging
+from unittest.mock import patch
+import pytest
+
+from homeassistant.const import PERCENTAGE
+from homeassistant.components.sensor import SensorStateClass, SensorDeviceClass
+from lennoxs30api.s30api_async import lennox_system, LENNOX_PRODUCT_TYPE_S40
+from custom_components.lennoxs30 import (
+    Manager,
+)
+from custom_components.lennoxs30.const import LENNOX_DOMAIN
+
+from custom_components.lennoxs30.sensor import WTEnvSensor, lennox_wt_env_sensors
+from tests.conftest import conftest_base_entity_availability, loadfile
+
+
+@pytest.mark.asyncio
+async def test_wt_env_sensor_text(hass, manager_system_04_furn_ac_zoning_ble: Manager):
+    """Test the alert sensor"""
+    manager = manager_system_04_furn_ac_zoning_ble
+    system: lennox_system = manager.api.system_list[0]
+    system.productType = LENNOX_PRODUCT_TYPE_S40
+    data = loadfile("weather.json", system.sysId)
+    manager.api.processMessage(data)
+
+    sensor_dict = lennox_wt_env_sensors[0]
+    sensor = WTEnvSensor(hass, manager, system, sensor_dict)
+
+    assert sensor.unique_id == (system.unique_id + "wt_env_airQuality").replace("-", "")
+    assert sensor.name == system.name + " " + sensor_dict["name"]
+    assert sensor.available is True
+    assert sensor.should_poll is False
+    assert sensor.update() is True
+    assert sensor.state_class is None
+    assert sensor.device_class is None
+    assert sensor.extra_state_attributes is None
+    assert sensor.native_value == "good"
+    assert sensor.entity_category is None
+    assert sensor.native_unit_of_measurement is None
+
+    identifiers = sensor.device_info["identifiers"]
+    for ids in identifiers:
+        assert ids[0] == LENNOX_DOMAIN
+        assert ids[1] == system.unique_id
+
+    system.wt_env_airQuality = "error"
+    assert sensor.available is False
+
+    system.wt_env_airQuality = "moderate"
+    assert sensor.available is True
+    assert sensor.native_value == "moderate"
+
+    system.wt_is_valid = False
+    assert sensor.available is False
+
+
+@pytest.mark.asyncio
+async def test_wt_env_sensor_humidity(hass, manager_system_04_furn_ac_zoning_ble: Manager):
+    """Test the wt_env sensosr"""
+    manager = manager_system_04_furn_ac_zoning_ble
+    system: lennox_system = manager.api.system_list[0]
+    system.productType = LENNOX_PRODUCT_TYPE_S40
+    data = loadfile("weather.json", system.sysId)
+    manager.api.processMessage(data)
+
+    sensor_dict = lennox_wt_env_sensors[6]
+    sensor = WTEnvSensor(hass, manager, system, sensor_dict)
+
+    assert sensor.unique_id == (system.unique_id + "wt_env_humidity").replace("-", "")
+    assert sensor.name == system.name + " " + sensor_dict["name"]
+    assert sensor.available is True
+    assert sensor.should_poll is False
+    assert sensor.update() is True
+    assert sensor.state_class == SensorStateClass.MEASUREMENT
+    assert sensor.device_class == SensorDeviceClass.HUMIDITY
+    assert sensor.extra_state_attributes is None
+    assert sensor.native_value == 84
+    assert sensor.native_unit_of_measurement == PERCENTAGE
+
+    identifiers = sensor.device_info["identifiers"]
+    for ids in identifiers:
+        assert ids[0] == LENNOX_DOMAIN
+        assert ids[1] == system.unique_id
+
+    system.wt_env_humidity = "error"
+    assert sensor.native_value is None
+
+    system.wt_env_humidity = 86
+    assert sensor.native_value == 86
+
+    system.wt_is_valid = False
+    assert sensor.available is False
+
+
+@pytest.mark.asyncio
+async def test_wt_env_subscription(hass, manager_system_04_furn_ac_zoning_ble: Manager, caplog):
+    """Test the alert sensor subscription"""
+    manager = manager_system_04_furn_ac_zoning_ble
+    system: lennox_system = manager.api.system_list[0]
+    system.productType = LENNOX_PRODUCT_TYPE_S40
+    data = loadfile("weather.json", system.sysId)
+    manager.api.processMessage(data)
+
+    sensor_dict = lennox_wt_env_sensors[6]
+    sensor = WTEnvSensor(hass, manager, system, sensor_dict)
+    await sensor.async_added_to_hass()
+
+    with caplog.at_level(logging.DEBUG):
+        with patch.object(sensor, "schedule_update_ha_state") as update_callback:
+            caplog.clear()
+            update = {"value": "65.4"}
+            system.attr_updater(update, "value", "wt_env_humidity")
+            system.executeOnUpdateCallbacks()
+            assert update_callback.call_count == 1
+            assert sensor.native_value == 65.0
+            assert len(caplog.messages) == 2
+            assert sensor.name in caplog.messages[1]
+            assert "sensor_value_update" in caplog.messages[1]
+
+        with patch.object(sensor, "schedule_update_ha_state") as update_callback:
+            caplog.clear()
+            update = {"value": False}
+            system.attr_updater(update, "value", "wt_is_valid")
+            system.executeOnUpdateCallbacks()
+            assert update_callback.call_count == 1
+            assert sensor.available is False
+            assert len(caplog.messages) == 2
+            assert sensor.name in caplog.messages[1]
+            assert "sensor_value_update" in caplog.messages[1]
+
+    update = {"value": True}
+    system.attr_updater(update, "value", "wt_is_valid")
+    system.executeOnUpdateCallbacks()
+    conftest_base_entity_availability(manager, system, sensor)
